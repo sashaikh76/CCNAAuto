e152439dd0629b3d84a9284d76ba830516471ecb
Salman Shaikh <salman.shaikh@outlook.com>
2026-02-05 22:39:01 -0600

    Added Congratulations message to the hello.py script

---
+++ Python_Codes/hello.py
@@ -0,0 +1,3 @@
+print ("Hello Network Engineers in World of Netowrk Automation")
+print ("Hello Network Engineers in World of Netowrk Automation")
+print ("Congratulations! You have successfully run your first Python Script")
